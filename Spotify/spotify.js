42eb0b5de93d2e902dfd3207535d85eea6e240bd
james <jab.bradley99@gmail.com>
2025-04-08 16:24:49 +0100

    create SpotifyHandler

--- Spotify/spotify.js
+++ Spotify/spotify.js
@@ -1,24 +1,45 @@
 const tokens = require("./tokens.json");
 
-function getAccessToken() {
-    const params = new URLSearchParams();
-    params.append("grant_type", "client_credentials");
-    params.append("client_id", tokens.client_id);
-    params.append("client_secret", tokens.client_secret);
+class SpotifyHandler {
+    constructor() {
+        this.access_token = "";
+    }
 
-    return fetch("https://accounts.spotify.com/api/token",{
-        method: "POST",
-        headers: {
-            "Conntent-Type": "application/x-www-form-urlencoded",
-        },
-        body: params
-    })
-    .then((response) => response.json())
-    .then(data => {
-        return data.access_token;
-    })
+    async getAccessToken() {
+        const params = new URLSearchParams();
+        params.append("grant_type", "client_credentials");
+        params.append("client_id", tokens.client_id);
+        params.append("client_secret", tokens.client_secret);
+
+        const response = await fetch("https://accounts.spotify.com/api/token",{
+            method: "POST",
+            headers: {
+                "Conntent-Type": "application/x-www-form-urlencoded",
+            },
+            body: params
+        });
+        const data = await response.json();
+        this.access_token = data.access_token;
+    }
+
+    async searchAlbums(searchText) {
+        const baseUrl = "https://api.spotify.com/v1/search";
+        const params = new URLSearchParams({
+            q: searchText,
+            type: "album",
+        });
+        const url = `${baseUrl}?${params}`;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${this.access_token}`,
+            }
+        });
+        return response.json();
+    }
 }
 
+
 module.exports = {
-    getAccessToken,
+    SpotifyHandler
 };
--- Spotify/spotify.test.js
+++ Spotify/spotify.test.js
@@ -1,12 +1,19 @@
-const {getAccessToken, SpotifyToken} = require("./spotify.js");
+const { response } = require("express");
+const {SpotifyHandler} = require("./spotify.js");
 
 describe("getAccessToken", () => {
-    test("should return an access token", () => {
-        return getAccessToken()
-        .then((token) => {
-            expect(token).toBeDefined();
-        })
-    });
-
-    
+    test("should return an access token", async () => {
+        const handler = new SpotifyHandler();
+        await handler.getAccessToken();
+        expect(handler.access_token).toBeDefined();
+    });    
 })
+
+describe("searchAlbums", () => {
+    test("should search for albums", async () => {
+        const handler = new SpotifyHandler();
+        await handler.getAccessToken();
+        const response = await handler.searchAlbums("Holy Fire")
+        console.log(response);
+    });
+});
