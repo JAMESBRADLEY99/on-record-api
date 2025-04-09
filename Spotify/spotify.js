23155801ed3afa57daf5db67762f4730c0fab4a1
james <jab.bradley99@gmail.com>
2025-04-09 15:11:35 +0100

    improve formatting of searchAlums

--- Spotify/spotify.js
+++ Spotify/spotify.js
@@ -10,6 +10,7 @@ class SpotifyHandler {
     params.append("grant_type", "client_credentials");
     params.append("client_id", tokens.client_id);
     params.append("client_secret", tokens.client_secret);
+
     console.log("fetching token");
 
     const response = await fetch("https://accounts.spotify.com/api/token", {
@@ -19,24 +20,25 @@ class SpotifyHandler {
       },
       body: params,
     });
+
     const data = await response.json();
     this.access_token = data.access_token;
   }
 
   async searchAlbums(searchText) {
     const baseUrl = "https://api.spotify.com/v1/search";
-    const params = new URLSearchParams({
-      q: searchText,
-      type: "album",
-    });
+    const params = new URLSearchParams({ q: searchText, type: "album" });
     const url = `${baseUrl}?${params}`;
+
     const response = await fetch(url, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${this.access_token}`,
       },
     });
+
     const data = await response.json();
+
     if ("albums" in data) {
       const items = data.albums.items;
       const albums = items.filter((item) => item.album_type === "album");
@@ -45,6 +47,7 @@ class SpotifyHandler {
         artist: album.artists[0].name,
       }));
     }
+
     if ("error" in data && data.error.status === 400) {
       await this.getAccessToken();
       return this.searchAlbums(searchText);
