e0a5755cdb77ab4f9440236a7a2d22be93ea6e98
James Bradley <jab.bradley99@gmail.com>
2025-05-21 14:19:19 +0100

    add cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const { searchAlbum } = require("./Controllers/controller");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("I'm here :)");
